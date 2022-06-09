0d9db1f00600892e758a4e41b6c0ab36ab233040
William Antwi-Boasiako <william.antwi-boasiako@persol.net>
2022-06-09 10:12:38 +0000

    connected to live db

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 const connectDb = () => {
-    mongoose.connect(process.env.NODE_EVN ==="production" ?  process.env.DB_PRODUCTION : process.env.DB_LOCAL, {
+    mongoose.connect(process.env.DB_PRODUCTION, {
     useNewUrlParser: true,
     useUnifiedTopology: true
  }).then(cons =>{
@@ -11,5 +11,5 @@ const connectDb = () => {
 
 module.exports = connectDb
 
-//
+//mongoose.connect(process.env.NODE_EVN ==="production" ?  process.env.DB_PRODUCTION : process.env.DB_LOCAL, {
     // mongoose.connect(process.env.DB_PRODUCTION, {
