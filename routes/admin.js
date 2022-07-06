8ce208dff12aac12b4a208f9853eadc176c09f28
William Antwi-Boasiako <william.antwi-boasiako@persol.net>
2022-07-06 13:24:58 +0200

    get user by type added

--- routes/admin.js
+++ routes/admin.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const cors = require('cors')
-const { registerUser, loginUser, adminTest, getUserProfile, uploadProfileImage, renewToken, usersStats } = require("../controllers/adminController");
+const { registerUser, loginUser, adminTest, getUserProfile, uploadProfileImage, renewToken, usersStats, AllUsersByType } = require("../controllers/adminController");
 
 const { authorizeRoles, isAdminAuthenticatedUser } = require("../middleware/auth");
 
@@ -14,7 +14,7 @@ router.route("/admin/register").post(isAdminAuthenticatedUser, authorizeRoles('a
 router.route("/admin/login").post(loginUser);
 router.route("/admin/refresh").get(renewToken);
 
-
+router.route("getUsers/:userType").get(AllUsersByType)
 router.route("/admin/user/me").get(isAdminAuthenticatedUser, getUserProfile);
 router.route("/admin/user/profileimage").post(isAdminAuthenticatedUser, uploadProfileImage);
 router.route('/users/stats').get(usersStats)
