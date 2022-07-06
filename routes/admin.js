564b2807d0379e9b9d8da32500dbf995e4796e7d
William Antwi-Boasiako <william.antwi-boasiako@persol.net>
2022-07-06 18:19:34 +0200

    path fix

--- routes/admin.js
+++ routes/admin.js
@@ -14,7 +14,7 @@ router.route("/admin/register").post(isAdminAuthenticatedUser, authorizeRoles('a
 router.route("/admin/login").post(loginUser);
 router.route("/admin/refresh").get(renewToken);
 
-router.route("getUsers/:userType").get(AllUsersByType)
+router.route("/getUsers/:userType").get(AllUsersByType)
 router.route("/admin/user/me").get(isAdminAuthenticatedUser, getUserProfile);
 router.route("/admin/user/profileimage").post(isAdminAuthenticatedUser, uploadProfileImage);
 router.route('/users/stats').get(usersStats)
